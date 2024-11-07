3d914daa6b7d7cb45b9588967f55d90cde696fe8
Asem496 <asemthepro@outlook.com>
2024-11-08 00:01:29 +0100

    first commit

---
+++ jemig.js/test.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 100;
+let c = 1000;
+
+// && = and
+// || = or
+
+if (a < b && b > c){
+    console.log("a is kleiner");
+}
+else if (a>b){
+    console.log("b is kleiner");
+}
+else{
+    console.log("ze zijn gelijk");
+}
---
+++ wiskunde/wiksunde.js
@@ -0,0 +1,6 @@
+function berekenInhoudkubus(lengte, breedte, hoogte) {
+    return lengte * breedte * hoogte;
+}
+
+let kubusInhoud = berekenInhoudkubus(10, 5, 10);
+console.log(kubusInhoud);
